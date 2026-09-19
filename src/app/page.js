0e19499b91a212e08a1ea66ba6b72f329829a692
MigSilverS <migupoke@gmail.com>
2026-09-19 18:05:51 -0300

    Simplificar o código

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,7 @@
-import logo from "../images/logo.png";
 import styles from "./page.module.css";
-import NavBar from "@/components/NavBar/NavBar";
+import NavBar from "@/app/components/NavBar/NavBar";
 import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
-import Footer from "@/components/Footer/Footer";
+import Footer from "@/app/components/Footer/Footer";
 
 export default function Home() {
   return (
