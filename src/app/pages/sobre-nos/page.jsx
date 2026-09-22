3a7816a560fe8527b9296abdb04964c9eff67135
danilo-alegre01 <daniloalegre.dev@gmail.com>
2026-09-22 17:27:26 -0300

    Atualiza seção sobre nós

--- src/app/pages/sobre-nos/page.jsx
+++ src/app/pages/sobre-nos/page.jsx
@@ -13,7 +13,7 @@ export default function SobreNos() {
         ),
 
         smile: (
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="" width={16} height={16}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="rounded-circle p-2" width={40} height={40} style={{backgroundColor: "#E3E8F9"}}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
             </svg>
 
@@ -49,6 +49,7 @@ export default function SobreNos() {
                                 <p>Escolas parceiras</p>
                             </div>
                             <div className="col-10 col-md-5 bg-white shadow-sm p-4 rounded">
+                                <div>{icons.smile}</div>
                                 <Titulo titulo="+3" nivel={2} />
                                 <p>Escolas parceiras</p>
                             </div>
