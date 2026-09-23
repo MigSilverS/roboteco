1993617090e0de0ed10e70f1409c6b3d5ffd51cb
danilo-alegre01 <daniloalegre.dev@gmail.com>
2026-09-23 12:12:33 -0300

    Sobre nós alinhada

--- src/app/pages/sobre-nos/page.jsx
+++ src/app/pages/sobre-nos/page.jsx
@@ -89,30 +89,38 @@ export default function SobreNos() {
                             <p>Conheça o time que faz o ROBOTECO acontecer. Reunimos diferentes conhecimentos e experiências para desenvolver uma plataforma que aproxima crianças e jovens da robótica, da programação e da eletrônica. Atuamos na criação dos conteúdos, na organização das atividades e na conexão com as escolas, buscando tornar o aprendizado mais acessível, prático e interessante.</p>
                         </div>
                         <div className="col-12 col-md-6">
-                            <div className="row row-cols-2 justify-content-md-end justify-content-center gap-3">
-                                <div className="col-10 col-md-5 bg-white shadow-sm p-4" style={{ borderRadius: '25px' }}>
-                                    <div className="mb-3">
-                                        {icons.robo}
+                            <div className="row justify-content-md-end justify-content-center g-3">
+                                <div className="col-5">
+                                    <div className="h-100 bg-white shadow-sm p-4"  style={{ borderRadius: '25px' }}>
+                                        <div className="mb-3">
+                                            {icons.robo}
+                                        </div>
+                                        <Titulo titulo="+10" nivel={2} />
+                                        <p>Robôs montados em nosso site</p>
                                     </div>
-                                    <Titulo titulo="+10" nivel={2} />
-                                    <p>Robôs montados em nosso site</p>
                                 </div>
-                                <div className="col-10 col-md-5 bg-white shadow-sm p-4" style={{ borderRadius: '25px' }}>
-                                    <div className="mb-3">{icons.chapeu}</div>
-                                    <Titulo titulo="+3" nivel={2} />
-                                    <p>Escolas parceiras</p>
-                                </div>
-                                <div className="col-10 col-md-5 bg-white shadow-sm p-4" style={{ borderRadius: '25px' }}>
-                                    <div className="mb-3">
-                                        {icons.head}
+                                <div className="col-5">
+                                    <div className="h-100 bg-white shadow-sm p-4"  style={{ borderRadius: '25px' }}>
+                                        <div className="mb-3">{icons.chapeu}</div>
+                                        <Titulo titulo="+3" nivel={2} />
+                                        <p>Escolas parceiras</p>
                                     </div>
-                                    <Titulo titulo="+3" nivel={2} />
-                                    <p>Escolas parceiras</p>
                                 </div>
-                                <div className="col-10 col-md-5 bg-white shadow-sm p-4" style={{ borderRadius: '25px' }}>
-                                    <div className="mb-3">{icons.smile}</div>
-                                    <Titulo titulo="+3" nivel={2} />
-                                    <p>Escolas parceiras</p>
+                                <div className="col-5">
+                                    <div className="h-100 bg-white shadow-sm p-4"  style={{ borderRadius: '25px' }}>
+                                        <div className="mb-3">
+                                            {icons.head}
+                                        </div>
+                                        <Titulo titulo="+3" nivel={2} />
+                                        <p>Escolas parceiras</p>
+                                    </div>
+                                </div>
+                                <div className="col-5">
+                                    <div className="h-100 bg-white shadow-sm p-4"  style={{ borderRadius: '25px' }}>
+                                        <div className="mb-3">{icons.smile}</div>
+                                        <Titulo titulo="+3" nivel={2} />
+                                        <p>Escolas parceiras</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
