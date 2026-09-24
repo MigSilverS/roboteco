4df69ed9c485772a2bfdf44352d79b20e5653d64
danilo-alegre01 <daniloalegre.dev@gmail.com>
2026-09-24 14:37:42 -0300

    nem sei que isso

--- src/app/pages/sobre-nos/page.jsx
+++ src/app/pages/sobre-nos/page.jsx
@@ -16,7 +16,7 @@ import logo from '@/app/images/wolfy-logo.png';
 import ana from '@/app/images/equipe/Ana.jpeg';
 import livia from '@/app/images/equipe/Livia.png';
 import diego from '@/app/images/equipe/Diego.png';
-import miguel from '@/app/images/equipe/Miguel.jpeg';
+import miguel from '@/app/images/equipe/Miguel.png';
 import thaina from '@/app/images/equipe/Thaina.png';
 
 
